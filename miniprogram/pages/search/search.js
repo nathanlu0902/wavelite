00270690ab6354c664d28fc4dcbf6cd4a5f59788
nathanlu0902 <nathan.lu0902@outlook.com>
2023-07-12 21:03:44 +0800

    2

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,66 +1,74 @@
-// pages/search/search.js
+const goodsList=wx.getStorageSync('goodsList')
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    default:"大地",
+    result_list:[]
+  },
+  onLoad(){
+    let search_history=wx.getStorageSync('searchHistory')||[]
+    let recommend=["test","test2"]
+    this.setData({
+      search_history:search_history,
+      recommend:recommend
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
+  onConfirmSearch(e){
+    let keyword=e.detail.value;
+    let history=wx.getStorageSync('searchHistory')
+    //不输入直接搜索，则搜索placeholder
+    if(keyword==""){
+      keyword=this.data.default;
+    }
+    //如果已经存在同样的搜索记录，将旧记录删除，头部添加新记录
+    if(history.length>0){
+      for(let i=0;i<history.length;i++){
+        if(history[i]===keyword){
+          history.splice(i,1)
+        }
+      }
+      history.unshift(keyword) 
+    }else{
+      history.unshift(keyword)
+    }
+    wx.setStorageSync('searchHistory', history)
 
+    //搜索逻辑
+    let result_list=[];
+    var reg=new RegExp(".*"+keyword+".*","i")
+    for(let i=0;i<goodsList.length;i++){
+      console.log(goodsList[i].category)
+      let res=reg.test(goodsList[i].category)
+      console.log(res)
+      if(res){
+        result_list.push.apply(result_list,goodsList[i].goodsList) //把goodslist数组赋值给结果数组
+      }else{
+        for(let j=0;j<goodsList[i].goodsList.length;j++){
+          let res=reg.test(goodsList[i].goodsList[j].goodsName);
+          if(res){
+            result_list.push(goodsList[i].goodsList[j])
+          }
+        }
+      }
+    }
+    this.setData({
+      result_list:result_list,
+      not_found:false
+    })
+    if(result_list.length==0){
+      this.setData({
+        not_found:true
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onCancel(){
+    this.setData({
+      keyword:"",
+      result_list:[]
+    })
+    
   }
 })
