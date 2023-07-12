f317b9f2644288908eee39b32a1b2afcf3839ae2
nathanlu0902 <nathan.lu0902@outlook.com>
2023-07-12 22:33:54 +0800

    2

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -7,41 +7,74 @@ Page({
     result_list:[]
   },
   onLoad(){
-    let search_history=wx.getStorageSync('searchHistory')||[]
-    let recommend=["test","test2"]
+    if(wx.getStorageSync('searchHistory')){
+      var search_history=wx.getStorageSync('searchHistory')
+    }else{
+      wx.setStorageSync('searchHistory', [])
+    }
+    // let search_history=wx.getStorageSync('searchHistory')||[]
+    var recommend=["test","test2"]
     this.setData({
       search_history:search_history,
       recommend:recommend
     })
   },
+  onInputChange(e){
+    let {value}=e.detail;
+    if(value==""){
+      this.setData({
+        keyword:"",
+        result_list:[]
+      })
+    }else{
+      this.search(value)
+    }
+  },
 
   onConfirmSearch(e){
     let keyword=e.detail.value;
-    let history=wx.getStorageSync('searchHistory')
     //不输入直接搜索，则搜索placeholder
     if(keyword==""){
       keyword=this.data.default;
     }
-    //如果已经存在同样的搜索记录，将旧记录删除，头部添加新记录
-    if(history.length>0){
-      for(let i=0;i<history.length;i++){
-        if(history[i]===keyword){
-          history.splice(i,1)
-        }
-      }
-      history.unshift(keyword) 
-    }else{
-      history.unshift(keyword)
-    }
-    wx.setStorageSync('searchHistory', history)
-
+    //根据keyword是否为空判断是否显示搜索历史和推荐
+     //如果已经存在同样的搜索记录，将旧记录删除，头部添加新记录
+     let history=wx.getStorageSync('searchHistory')
+     if(history.length>0){
+       for(let i=0;i<history.length;i++){
+         if(history[i]===keyword){
+           history.splice(i,1)
+         }
+       }
+       history.unshift(keyword) 
+     }else{
+       history.unshift(keyword)
+     }
+     wx.setStorageSync('searchHistory', history)
+     this.setData({
+       search_history:history,
+       keyword:keyword
+     })
+    
     //搜索逻辑
+    this.search(keyword);
+
+  },
+
+  onCancel(){
+    this.setData({
+      keyword:"",
+      result_list:[]
+    })
+    
+  },
+
+  search(keyword){
     let result_list=[];
     var reg=new RegExp(".*"+keyword+".*","i")
+
     for(let i=0;i<goodsList.length;i++){
-      console.log(goodsList[i].category)
       let res=reg.test(goodsList[i].category)
-      console.log(res)
       if(res){
         result_list.push.apply(result_list,goodsList[i].goodsList) //把goodslist数组赋值给结果数组
       }else{
@@ -54,21 +87,14 @@ Page({
       }
     }
     this.setData({
-      result_list:result_list,
-      not_found:false
+      result_list:result_list
     })
-    if(result_list.length==0){
-      this.setData({
-        not_found:true
-      })
-    }
   },
 
-  onCancel(){
+  empty_history(){
     this.setData({
-      keyword:"",
-      result_list:[]
+      search_history:[]
     })
-    
+    wx.setStorageSync('searchHistory', [])
   }
 })
