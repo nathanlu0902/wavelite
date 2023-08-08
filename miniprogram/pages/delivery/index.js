5c3f353455ba4c6b64ae5ef7b33bed8d562d137a
nathanlu0902 <nathan.lu0902@outlook.com>
2023-08-08 20:30:26 +0800

    2

--- miniprogram/pages/delivery/index.js
+++ miniprogram/pages/delivery/index.js
@@ -76,24 +76,24 @@ Page({
         type:"get"
       }
     }).then(res=>{
-      category=res.result.data
-      //初始化所有分类的默认type为单品
-      for(let i=0;i<category.length;i++){
-        var category_item=category[i];
-        let category_id=Object.keys(category_item)[1] //获取分类名称，比如poke
-        category_obj[category_id]={} //初始化
-        let end=category_item[category_id].goodsList.length;
-        for(let index=0;index<end;index++){
-          let good=category_item[category_id].goodsList[index];
+      //得到一个数组，每一项为category对象
+      categoryList=res.result.data
+      for(let index in categoryList){
+        //获取category对象
+        var category_obj=categoryList[index];
+        //获取category对象的key，即poke or xxx
+        let category_name=Object.keys(category_item)[1]
+        // category_obj[category_id]={} //初始化
+        //获取category的goodsList
+        let goodsList=category_obj.category_name.goodsList;
+
+        for(let index=0;index<goodsList.length;index++){
+          let good=goodsList[index]
           //将stock=0的项移至末位
           if(good.stock===0){
-            console.log(index,good)
-            category_item[category_id].goodsList.push(category_item[category_id].goodsList.splice(index,1)[0]);
+            goodsList.push(goodsList.splice(index,1)[0]);
             //array数据往前进一，避免跳过
-            // index-=1;
-            //避免最后数字重复排序
-            end-=1;
-            console.log(index,good)
+            index-=1;
           }
           
           //更新价格,加上标配价格
@@ -102,17 +102,17 @@ Page({
               good.goodsPrice+=good.material["标配"][k]
             }
           }
-          good.category_id=category_id
+          good.category_name=category_name
           good.temp_qty=1
           
-          category_obj[category_id][good.id]=good //不能直接[category_id][good.id]=good,会报cant set property of undefined 错误
+          // category_obj[category_id][good.id]=good //不能直接[category_id][good.id]=good,会报cant set property of undefined 错误
         }
         }
       //查找购物车，更新spu_qty
       let cart=wx.getStorageSync('cart')
       for(let i=0;i<cart.length;i++){
         let cart_item=cart[i]
-        category_obj[cart_item.category_id][cart_item.id].spu_qty+=cart_item.sku_qty
+        category_obj[cart_item.category_name][cart_item.id].spu_qty+=cart_item.sku_qty
       }
       wx.setStorageSync('category_obj', category_obj)
       this.setData({
